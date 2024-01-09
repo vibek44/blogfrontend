d9954615363746414c28e5e73e7d1589e8fc87a7
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-09 17:42:28 +0200

    adding Loginform  helper function

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,17 @@ import Loginform from './component/Loginform'
 const App=()=> {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
+
+  const handleLogin =(event)=>{
+    event.preventDefault()
+  }
   return (
    <Loginform 
    username={username} 
    password={password} 
    setUsername={setUsername}
-   setPassword={setPassword}/>
+   setPassword={setPassword}
+   handleLogin={handleLogin} />
   )
 }
 
