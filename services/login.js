d5084ca80d6a80d41b00ef87287e516138b16bb5
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-09 19:43:51 +0200

    changing baseurl to make request

--- services/login.js
+++ services/login.js
@@ -1,9 +1,13 @@
 import axios from 'axios'
-const baseUrl='/api/login'
+const baseUrl = 'http://localhost:3003/api/login'
 
-const login=async credentials => {
-  const response =await axios.post(baseUrl, credentials)
+const login= async credentials => {
+  const response = await axios.post(baseUrl, credentials)
+  console.log(response);
+  
   return response.data
 }
 
-export default login
+export default {
+  login
+}
