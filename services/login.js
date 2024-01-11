32545548eb310bb2b147e853bc38e863e681ccd2
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-11 19:59:56 +0200

    removing console.log part

--- services/login.js
+++ services/login.js
@@ -1,10 +1,9 @@
 import axios from 'axios'
-const baseUrl = 'http://localhost:3003/api/login'
+const baseUrl = '/api/login'
 
 const login= async credentials => {
   const response = await axios.post(baseUrl, credentials)
-  console.log(response);
-  
+  console.log( response.data)
   return response.data
 }
 
