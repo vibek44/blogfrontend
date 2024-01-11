9b558b58aad651aa7da7f9d614bf91333351b268
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-11 20:01:08 +0200

    adding submit button element

--- src/component/Loginform.jsx
+++ src/component/Loginform.jsx
@@ -1,25 +1,25 @@
-const Loginform=({ username, password,setUsername,setPassword, handleLogin}) => {
+const Loginform=( { username, password,setUsername,setPassword, handleLogin } ) => {
   return(<form onSubmit={handleLogin}>
-        <div>
-          username
-            <input
-            type="text"
-            value={username}
-            name="Username"
-            onChange={({ target }) => setUsername(target.value)}
-          />
-        </div>
-        <div>
-          password
-            <input
-            type="password"
-            value={password}
-            name="Password"
-            onChange={({ target }) => setPassword(target.value)}
-          />
-        </div>
-        <button type="submit">login</button>
-      </form>)
+    <div>
+    username
+      <input
+        type="text"
+        value={username}
+        name="Username"
+        onChange={( { target } ) => setUsername(target.value)}
+      />
+    </div>
+    <div>
+      password
+      <input
+        type="password"
+        value={password}
+        name="Password"
+        onChange={({ target }) => setPassword(target.value)}
+      />
+    </div>
+    <button type="submit">login</button>
+  </form>)
 }
 
 export default Loginform
