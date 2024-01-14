077bf22138cb9058813687567af9b64f62db2fc2
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-14 16:09:24 +0200

    destructuring props, passing props to Blogform,function handleBlog added

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -1,11 +1,45 @@
+import { useState } from 'react'
 import Blogform from './Blogform'
+import Blog from './Blog'
+import blogService from '../../services/blog'
 
-const Dashboard=(props)=>{
+const Dashboard=({user,blogs,setBlogs,setErrorMessage,handleSignout})=>{
+  const[title,setTitle]=useState('')
+  const[author,setAuthor]=useState('')
+  const[url,setUrl]=useState('')
+
+  const handleBlogAdd=async(event)=>{
+    event.preventDefault()
+    const newBlog={title,author,url}
+    try{
+      const blog =await blogService.create(newBlog)
+      setBlogs(blogs.concat(blog))
+      setTitle('') 
+      setAuthor('')
+      setUrl('')
+    }catch(error){
+      setErrorMessage(error.response.data.error)
+      setTimeout(()=>{
+        setErrorMessage(null)
+      }, 3000)
+    }
+  }
+
   return(
     <>
-      <p>Logged in {props.user.username}</p>
-      <h3>Add blog</h3>
-      <Blogform />
+      <p>Logged in {user.username} <button onClick={ handleSignout }>
+        logout</button>
+      </p>
+      <Blogform  
+        title={title} setTitle={setTitle}
+        author={author} setAuthor={setAuthor}
+        url={url} setUrl={setUrl}
+        handleBlogAdd={handleBlogAdd}
+      />
+      <h3>Blogs</h3>
+      {
+        blogs.map( blog=> <Blog key={blog.id} blog={blog}/> )
+      }
     </>
   )
 }
