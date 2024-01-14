4d878a09ca27d63f831510c9c7261cfa1044e041
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-14 18:15:24 +0200

    adding component Notification to render messages

---
+++ src/component/Notification.jsx
@@ -0,0 +1,15 @@
+import '../Styles/notification.css'
+
+const Notification=({message})=> {
+  if(message.notification===null && message.errorMessage===null){
+    return null
+  }else if(message.notification){
+    return(<p className='notification'>{message.notification}</p>)
+  }
+  else if(message.errorMessage){
+    return(<p className='error'>{message.errorMessage}</p>)
+  }
+
+}
+
+export default Notification
