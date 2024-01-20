98123109faf73d0ecb594e607309b2d8926c6741
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-20 20:35:46 +0200

    adding setToken,create,update,remove function services and exporting as object

--- services/blog.js
+++ services/blog.js
@@ -1,14 +1,46 @@
 import axios from 'axios'
-const baseUrlrl ='/api/blogs'
+const baseUrl ='/api/blogs'
 
+let token=null
 
+const setToken=newToken => {
+  token=`bearer ${newToken}`
+}
 
-const getAll=async ()=>{
-  const request= await axios.get(baseUrlrl)
+const getAll=async () => {
+  const request= await axios.get(baseUrl)
+  return request.data
+}
+
+const create=async (blogObject) => {
+  const config={
+    headers:{ authorization: token }
+  }
+  const request=await axios.post(baseUrl,blogObject,config)
+  return request.data
+}
+
+const update = async(id,updatedBlog) => {
+  const config={
+    headers:{ authorization: token }
+  }
+  const request=await axios.put(`${baseUrl}/${id}`,updatedBlog,config)
+  return request.data
+}
+
+const remove=async(id) => {
+  const config={
+    headers:{ authorization: token }
+  }
+  const request=await axios.delete(`${baseUrl}/${id}`,config)
   return request.data
 }
 
 export default {
   getAll,
- 
+  setToken,
+  create,
+  update,
+  remove
 }
+
