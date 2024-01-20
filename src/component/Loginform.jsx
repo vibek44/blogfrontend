ac38a25b45c6af26689770a1fb71b6bfce55a7f2
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-20 20:28:01 +0200

    using prop-types package to enforce component passes props in defined way

--- src/component/Loginform.jsx
+++ src/component/Loginform.jsx
@@ -1,25 +1,40 @@
+import propTypes from 'prop-types'
+
 const Loginform=( { username, password,setUsername,setPassword, handleLogin } ) => {
-  return(<form onSubmit={handleLogin}>
-    <div>
-    username
-      <input
-        type="text"
-        value={username}
-        name="Username"
-        onChange={( { target } ) => setUsername(target.value)}
-      />
-    </div>
-    <div>
-      password
-      <input
-        type="password"
-        value={password}
-        name="Password"
-        onChange={({ target }) => setPassword(target.value)}
-      />
-    </div>
-    <button type="submit">login</button>
-  </form>)
+  return(
+    <fieldset>
+      <legend>Login to application</legend>
+      <form onSubmit={handleLogin}>
+        <div>
+        username
+          <input
+            type="text"
+            value={username}
+            name="Username"
+            onChange={( { target } ) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+            type="password"
+            value={password}
+            name="Password"
+            onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type="submit">login</button>
+      </form>
+    </fieldset>)
+
+}
+
+Loginform.propTypes={
+  username:propTypes.string.isRequired,
+  password:propTypes.string.isRequired,
+  setUsername:propTypes.func.isRequired,
+  setPassword:propTypes.func.isRequired,
+  handleLogin:propTypes.func.isRequired
 }
 
 export default Loginform
