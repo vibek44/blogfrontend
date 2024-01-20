6e5859adf4a7330e8d18fc7b40b26ae029647e19
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-20 19:25:03 +0200

    creating Tooglable component

---
+++ src/component/Togglable.jsx
@@ -0,0 +1,37 @@
+
+import { useState,forwardRef,useImperativeHandle } from 'react'
+import propTypes from 'prop-types'
+
+const Togglable = forwardRef ( (props,refs) => {
+  const [blogFormVisible,setblogFormVisible]=useState(false)
+  const hideWhenVisible={ display:blogFormVisible? 'none' : '' }
+  const showWhenVisible={ display:blogFormVisible? '' : 'none' }
+
+  const toggleVisibility=() => {
+    setblogFormVisible(!blogFormVisible)
+  }
+  useImperativeHandle(refs,() => {
+    return{
+      toggleVisibility
+    }
+  })
+
+  return(
+    <div>
+      <div style={hideWhenVisible}><button onClick={toggleVisibility}>{props.buttonlabel}</button></div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  )
+}
+)
+
+Togglable.propTypes={
+  buttonlabel:propTypes.string.isRequired
+}
+
+Togglable.displayName='Togglable'
+
+export default Togglable
