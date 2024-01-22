6acaa536eaa80649edcbd44c1c2221f42a7c5669
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-22 20:14:39 +0200

    adding test to veriyy likes and url is rendered on button click

--- tests/Blog.test.js
+++ tests/Blog.test.js
@@ -1,9 +1,10 @@
-import Reac from 'react'
+import React from 'react'
 import '@testing-library/jest-dom'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from '../src/component/Blog'
 
-test('component renders content',() => {
+test('component renders title and author by default',() => {
 
   const blog={
     title:'The moon is shaded by the sun',
@@ -18,5 +19,35 @@ test('component renders content',() => {
   expect(div2).toHaveTextContent(
     'albert krishna',
   )
+})
+
+test.only('component renders url and likes when button is clicked for details',async() => {
+
+  const blog={
+    title: 'Curbs tips on Sync and async executions',
+    author: 'Geeks o Geeks',
+    url: 'https://www.geeksforgeeks.org/synchronous-and-asynchronous-in-javascript/',
+    likes: 29,
+    user: {
+      username: 'chhetri',
+      name: 'chhetri',
+    },
+  }
+  const user= {
+    username: 'chhetri',
+    name: 'chhetri',
+    id: '659adc49556707640dee96a8'
+  }
+  render(<Blog blog={blog} user={user}  />)
+
+  const user1=userEvent.setup()
+  const button=screen.getByText('view')
+  await user1.click(button)
+  const element=screen.getByText('https://www.geeksforgeeks.org/synchronous-and-asynchronous-in-javascript/')
+  const element2=screen.getByText('29')
+  screen.debug(element)
+  expect(element).toBeDefined()
+  expect(element2).toBeDefined()
 
 })
+
