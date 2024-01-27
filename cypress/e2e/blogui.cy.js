d66868eb20d16eff24e2abb329395d96b05b87df
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-27 21:04:27 +0200

    adding db reset request and user add in beforeEach test

--- cypress/e2e/blogui.cy.js
+++ cypress/e2e/blogui.cy.js
@@ -1,20 +1,43 @@
 describe('Blog App', function(){
   beforeEach(function(){
-    cy.visit('http://localhost:5173/')
+    cy.request('POST', 'http://localhost:3003/api/testing/resetdb')
+    const user={
+      name:'bhim chhetri',
+      username:'arian',
+      password:'Vchhetri123'
+    }
+    cy.request( 'POST', 'http://localhost:3003/api/users', user )
+    cy.visit( 'http://localhost:5173/' )
   })
 
-  it('blog front page is opened and has username and password text', function(){
-    cy.contains('username')
-    cy.contains('password')
+  it('login form is shown and has username and password text', function(){
+
+    cy.get('form')
+      .should('contain','username')
+      .should('contain','password')
+      .should('contain','login')
+    //cy.contains('username')
+    //cy.contains('password')
+    //cy.contains('login')
+
   })
-
-  it.only('user can login',function(){
-    cy.contains('login').click()
-    cy.get('input:first').type('chhetri')
-    cy.get('input:last').type('chhetri123')
-    cy.get('#submitbutton').click()
-    cy.contains('Logged in chhetri')
-
+  describe('When logged in', function(){
+    beforeEach(function(){
+      cy.get('input:first').type('arian')
+      cy.get('input:last').type('Vchhetri123')
+      cy.contains('login').click()
+    })
+    it('user can login',function(){
+      cy.contains('Logged in arian')
+    })
+    it.only('a blog can be created',function(){
+      cy.contains('Create newblog').click()
+      cy.get('#title').type('The Script is to execute')
+      cy.get('#author').type('grisma ritu')
+      cy.get('#url').type('https://docs.cypress.io/guides/end-to-end-testing/writing-your-first-end-to-end-test')
+      cy.get('#blogsubmit').click()
+      cy.contains('The Script is to execute')
+    })
   })
 
 })
