904348727cc3436b36826b17a305dd5a8dade357
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-28 15:18:13 +0200

    adding id to input and button element for e2e test cypress

--- src/component/Blogform.jsx
+++ src/component/Blogform.jsx
@@ -1,27 +1,43 @@
+import { useState } from 'react'
+const Blogform=({ handleBlogAdd }) => {
+  const[title,setTitle]=useState('')
+  const[author,setAuthor]=useState('')
+  const[url,setUrl]=useState('')
 
-const Blogform=({ title,author,url,setTitle,setAuthor,setUrl,addBlog }) => {
-
+  const addBlog=(event) => {
+    event.preventDefault()
+    handleBlogAdd({ title,author,url })
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
   return(
     <form onSubmit={addBlog}>
       <div>
         Title:<input
+          id='title'
           type='text'
           value={title}
+          placeholder='write title here'
           onChange={({ target }) => setTitle(target.value)} />
       </div>
       <div>
         Author:<input
+          id='author'
           type='text'
           value={author}
+          placeholder='author here'
           onChange={({ target }) => setAuthor(target.value)} />
       </div>
       <div>
         url:<input
+          id='url'
           type='text'
           value={url}
+          placeholder='url here'
           onChange={({ target }) => setUrl(target.value)} />
       </div>
-      <button type="submit">Create</button>
+      <button id='blogsubmit' type='submit'>Create</button>
     </form>
 
   )
