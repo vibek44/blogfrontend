bef80148dfde4791dc415182e0a54ed4a9f805c3
vibek44 <vibekkshetri@vibeks-MacBook-Pro.local>
2024-01-31 19:38:51 +0200

    adding className to elements for testing purpose

--- src/component/Blog.jsx
+++ src/component/Blog.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 
-const Blog=({ blog,user,handleBlogLike,handleBlogRemove }) => {
+const Blog=({ blog,user,handleBlogLike,handleBlogRemove, }) => {
   const [showDetail,setShowDetail]=useState(false)
   const blogStyle={
     paddingTop: 10,
@@ -26,16 +26,16 @@ const Blog=({ blog,user,handleBlogLike,handleBlogRemove }) => {
     })
   }
   return(
-    <div style={blogStyle} >
-      <button onClick={handleShow}>{showDetail?'hide':'view'}</button>
+    <div style={blogStyle}  >
+      <button className='viewhidebutton' onClick={handleShow}>{showDetail?'hide':'view'}</button>
       {showDetail
-        ?<div>
-          <p> { blog.title } </p>
+        ?<div className='blogdetail'>
+          <p className='blogtitle'> { blog.title } </p>
           <p> { blog.url }</p>
           <p> { blog.author } </p>
-          <p><button onClick={handleEdit}>like</button> {blog.likes}</p>
+          <p><button className='likebutton' onClick={handleEdit}>like</button> {blog.likes}</p>
           <p>{blog.user.name}</p>
-          {(blog.user.username===user.username)&&<button onClick={() => handleBlogRemove(blog)}>remove</button>}
+          {(blog.user.username===user.username)&&<button  onClick={() => handleBlogRemove(blog)}>remove</button>}
         </div>
         :<div>
           <p className='title'> {blog.title} </p>
